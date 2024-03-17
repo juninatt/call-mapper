805d8f3a30478c5b07089ed5a1a8d23f43278097
Petter B <petter.bergstrom@protonmail.com>
2024-03-17 21:51:46 +0100

    Increased collected data detail and enhanced result presentation

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -1,9 +1,13 @@
 def analyze_data(df_valid):
+    # Compile dataset statistics and analyze API call patterns.
     results = {
         'row_count': len(df_valid),
         'earliest_date': df_valid['@timestamp'].min(),
         'latest_date': df_valid['@timestamp'].max(),
-        'sorted': df_valid['@timestamp'].is_monotonic_increasing
+        'sorted': df_valid['@timestamp'].is_monotonic_increasing,
+        'unique_calls': df_valid['fields.RequestPath'].nunique(),
+        # Calculate and store the percentage of the top 10 most common API calls.
+        'top_10_calls_percentage': df_valid['fields.RequestPath'].value_counts(normalize=True).head(10).to_dict()
     }
-    return results
 
+    return results
