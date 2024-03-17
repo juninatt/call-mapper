14dbde2f3b9dd49cffc6546a8cb9d2cbfce29bc7
Petter B <petter.bergstrom@protonmail.com>
2024-03-17 20:31:32 +0100

    Organize files in sub packages

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from data_loader import load_data
-from data_processor import preprocess_data
-from data_analyzer import analyze_data
-from result_handler import save_results
+from src.data_loader import load_data
+from src.data_processor import preprocess_data
+from src.data_analyzer import analyze_data
+from src.result_handler import save_results
 
 if __name__ == "__main__":
-    filepath = 'api_calls_collected_traffic_iso8601.csv'
-    results_file = 'report.txt'
+    filepath = 'data/api_calls_collected_traffic_iso8601.csv'
+    results_file = 'data/report.txt'
 
     df = load_data(filepath)
     df_valid, invalid_timestamps = preprocess_data(df)
